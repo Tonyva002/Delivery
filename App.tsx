0f2193ba5b15e1c784c4a6c2b1fc7866cee898c9
Tony Vasquez <tonyva002@hotmail.com>
2024-07-09 17:51:54 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+      source={require('./assets/chef.jpg')}
+      style={styles.imageBackground} />
+
+      <View style={styles.form} >
+
+        </View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+ 
+  },
+
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+    bottom: '30%'
+  },
+
+  form: {
+    width: '100%',
+    height: '40%',
+    backgroundColor: 'white', 
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40
+    
+  }
+});
